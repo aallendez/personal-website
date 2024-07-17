ff7d8d17a91e44f12822cf8136aac04fb36a27b5
Juan <157056492+aallendez@users.noreply.github.com>
2024-07-17 12:32:15 +0200

    Update AboutMe.jsx

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,7 +2,7 @@ import { TailwindIcon, ReactIcon, PythonIcon, MongoDBIcon, PremiereProIcon, Afte
 
 const AboutMe = ({ iconColor }) => {
     return(
-      <div id="aboutMe" className="pt-32 w-full min-h-screen flex flex-col-reverse sm:flex-col-reverse md:flex-col-reverse xs:flex-col-reverse text-center gap-20 items-start md:px-0 sm:px-0 xs:px-0" >
+      <div id="aboutMe" className="pt-32 w-full min-h-screen flex flex-col-reverse sm:flex-col-reverse md:flex-col-reverse xs:flex-col-reverse text-center gap-20 items-start md:px-4" >
         <div className="w-full group">
           <div className="pb-6">
             <h3 className='text-3xl font-bold'>My Tech Stack</h3>
